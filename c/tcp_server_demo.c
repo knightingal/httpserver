74c064a25689f9a73e2a1f3c2af3ebd2a0ba0094
knightingal <yujianming1985@hotmail.com>
2014-07-06 15:44:18 +0800

    base send and recv

--- c/tcp_server_demo.c
+++ c/tcp_server_demo.c
@@ -1,36 +1,55 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
-#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+char *resp_buff = "\
+HTTP/1.1 200 OK\r\n\
+Server: nginx/0.8.55\r\n\
+Content-Length: 6\r\n\
+Content-Type: text/html\r\n\
+\r\n\
+123456";
+
 
 int main(int argc, char** argv)
 {
     int server_sockfd, client_sockfd;
     int server_len, client_len;
-    struct sockaddr_un server_address;
-    struct sockaddr_un client_address;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
 
     unlink("server_socket");
-    server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    server_address.sun_family = AF_UNIX;
-    strcpy(server_address.sun_path, "server_socket");
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(8081);
     server_len = sizeof(server_address);
-    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+    int ret = bind(
+            server_sockfd, 
+            (struct sockaddr *)&server_address, 
+            server_len
+    );
     listen(server_sockfd, 5);
     while(1)
     {
-        char ch;
+        //char ch;
+        char buff[2048] = {0};
 
         printf("server_waiting\n");
         client_len = sizeof(client_address);
         client_sockfd = accept(server_sockfd, 
                 (struct sockaddr*) &client_address, &client_len);
-        read(client_sockfd, &ch, 1);
-        ch++;
-        write(client_sockfd, &ch, 1);
+        read(client_sockfd, buff, 2048);
+//        ch++;
+//        write(client_sockfd, &ch, 1);
+        printf("buff = %s\n", buff);
+        write(client_sockfd, resp_buff, strlen(resp_buff));
+
         close(client_sockfd);
     }
 }
---
+++ python_client/client.py
@@ -0,0 +1,22 @@
+__author__ = 'knightingal'
+
+import urllib2
+
+if __name__ == "__main__":
+    request = urllib2.Request("http://127.0.0.1:8081/index")
+    request.add_header(
+        "User-Agent",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 "
+        "(KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31"
+    )
+    request.add_header("Connection", "keep-alive")
+    request.add_header("Accept", "*/*")
+    request.add_header("Accept-Encoding", "gzip,deflate,sdch")
+    request.add_header("Accept-Language", "zh-CN,zh;q=0.8")
+    request.add_header("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
+
+    request.add_data("hello")
+    fd = urllib2.urlopen(request)
+    net_string = fd.read()
+    fd.close()
+    print net_string
